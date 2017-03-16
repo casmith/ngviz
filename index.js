b4859e981b91bc8a7a0eaa5aea42f6725d6774b5
Clay Smith <uhclay@gmail.com>
2017-03-15 22:24:51 -0500

    Transform DI json and render

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 'use strict';
 
-var d3 = require('d3');
-var jsdom = require('jsdom');
+var d3 = require('d3'),
+	jsdom = require('jsdom'),
+	_ = require('lodash');
+
+function transformData(json) {
+	return {
+		name: 'app', 
+		children: _.map(json, (obj, key) => {
+				var children = _.map(obj, function (o) {
+					return o;
+				});
+				return {
+					name: key, 
+					size: children.length
+				};
+		})
+	};
+}
 
 jsdom.env({
 	html:'',
@@ -24,7 +40,10 @@ jsdom.env({
 			.append("g")
 			.attr("transform", "translate(-.5,-.5)");
 
-		var json = require('./test.json');
+		var json = require('./all-di.json');
+		json = transformData(json);
+		
+
 		var cell = svg.data([json]).selectAll("g")
 			.data(treemap.nodes)
 			.enter().append("g")
@@ -34,17 +53,18 @@ jsdom.env({
 		cell.append("rect")
 			.attr("width", function(d) { return d.dx; })
 			.attr("height", function(d) { return d.dy; })
-			.style("fill", function(d) { return d.children ? color(d.name) : null; });
+			.style("fill", function(d) { return color(d.size) });
 
 		cell.append("text")
 			.attr("x", function(d) { return d.dx / 2; })
 			.attr("y", function(d) { return d.dy / 2; })
 			.attr("dy", ".35em")
 			.attr("text-anchor", "middle")
-			.text(function(d) { return d.children ? null : d.name; });
+			.text(function(d) { return d.children ? null : d.name + '(' + d.size + ')'; });
 
 		console.log('<!DOCTYPE html><meta charset="utf-8"><style>rect {fill: none;stroke: #fff;}text {font: 10px sans-serif;}</style><body>' 
 			+ d3.select(window.document.body).html() 
 			+ '</body>');
 	}
+
 });
